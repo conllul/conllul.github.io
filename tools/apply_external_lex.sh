299b28c8733613ceeaefc1ce033cf0c83a1c3b82
Amir More <habeanf@gmail.com>
2018-03-25 23:16:48 +0300

    Add script to run external lexicon-based ma

---
+++ tools/apply_external_lex.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+YAP=../yap
+for lexicon in `find . -name 'UDLex*conllul'`; do
+    treebank=`echo $lexicon | cut -d '/' -f 2`
+    lex_file=`echo $lexicon | cut -d '/' -f 3`
+    lex_name=`echo $lex_file | cut -d '.' -f 1 | cut -d '-' -f 2-`
+    echo Applying $lex_file to treebank $treebank
+    pushd "$treebank/" > /dev/null
+    for tb_file in `ls -1 *conllu`; do
+        dict_file=`echo $tb_file | cut -d '-' -f 1`
+        file_prefix=`echo $tb_file | cut -d '.' -f 1`
+        file_split=`echo $file_prefix | cut -d '-' -f 3`
+        printf "\tRunning lexicon-based MA on file %s\n" "$tb_file"
+        echo $YAP ma -conllu $tb_file -dict $dict_file.json -udlex $lex_file -format ud -out $file_prefix.$lex_name.conllul ">" $file_split-ma.$lex_name.log
+        $YAP ma -conllu $tb_file -dict $dict_file.json -udlex $lex_file -format ud -out $file_prefix.$lex_name.conllul 2> $file_split-ma.$lex_name.log
+    done
+    popd > /dev/null
+done
