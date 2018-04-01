b6a1b22fe02ba3578ee313d7d4101bf62ce65a6b
Amir More <habeanf@gmail.com>
2018-04-01 21:08:43 +0300

    Remove initial settings for git push

--- tools/gitpush.sh
+++ tools/gitpush.sh
@@ -4,7 +4,6 @@ set -x
 for d in `ls -1d UL*`; do
     echo $d;
     pushd $d/ > /dev/null;
-    git branch --set-upstream-to=origin/master master;
-    git push --force --set-upstream origin master
+    git push
     popd > /dev/null;
 done
