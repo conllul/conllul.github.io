8b2edc31ca7c5b6d751aa2183d781c0665531ca4
Amir More <habeanf@gmail.com>
2018-04-02 01:44:36 +0300

    More fixes to coverage script for malformed conllul files

--- tools/calc_coverage.py
+++ tools/calc_coverage.py
@@ -42,6 +42,8 @@ def get_spellout_set(lattice, func):
 def calc_lattice_coverage(spellout, lattice, extraction_func):
     lattice_spellouts = get_spellout_set(lattice, extraction_func)
     projected_spellout = get_projected(spellout, extraction_func)
+    # pprint(lattice_spellouts, width=240)
+    # pprint(projected_spellout, width=240)
     result = 1 if projected_spellout in lattice_spellouts else 0
     return result
 
@@ -49,17 +51,71 @@ def calc_lattice_coverage(spellout, lattice, extraction_func):
 def conllul_as_lattices(raw_sent):
     lattices = []
     cur_lattice = defaultdict(list)
-    cur_lattice_token = ""
+    cur_lattice_token = ("", "", "")
+    cur_lattice_end = -1
+    cur_lattice_start = 0
     for line in raw_sent:
+        # print "At line %s" % str(line)
         if '-' in line[0]:
+            # encountered explicitly multi segment range line
+            # print "At new range %s" % str(line)
             if cur_lattice:
-                lattices.append((cur_lattice_token[2], cur_lattice, cur_lattice_token[0]))
+                # if a lattice exists, add it
+                misc_field = cur_lattice_token[2] if len(cur_lattice_token) > 2 else ""
+                # print "Appending existing %s" % str((misc_field, cur_lattice, cur_lattice_token[0]))
+                lattices.append((misc_field, cur_lattice, "%s-%s" % (cur_lattice_start, cur_lattice_end)))
+            # print "Start new lattice for %s" % line[1]
             cur_lattice = defaultdict(list)
             cur_lattice_token = line
+            line_split = line[0].split('-')
+            cur_lattice_start = int(line_split[0])
+            cur_lattice_end = int(line_split[1])
             continue
-        cur_lattice[line[0]].append(line)
+        if line[6] == "None":
+            line[6] = "_"
+        cur_line_start = int(line[0])
+        cur_line_end = int(line[1])
+        if cur_line_start >= cur_lattice_start and cur_line_start < cur_lattice_end and cur_line_end <= cur_lattice_end:
+            # print "Add edge to existing lattice"
+            # encountered line from in-progress lattice
+            cur_lattice[line[0]].append(line)
+            continue
+        if cur_lattice_start == cur_line_start:
+            # line is a previously un-encountered lattice
+            if cur_lattice:
+                # if a lattice exists, add it
+                misc_field = cur_lattice_token[2] if len(cur_lattice_token) > 2 else ""
+                # print "Appending existing %s" % str((misc_field, cur_lattice, "%s-%s" % (cur_lattice_start, cur_lattice_end)))
+                lattices.append((misc_field, cur_lattice, "%s-%s" % (cur_lattice_start, cur_lattice_end)))
+            # print "Start new lattice for %s" % line[2]
+            cur_lattice = defaultdict(list)
+            cur_lattice_token = line
+            cur_lattice_start = int(line[0])
+            cur_lattice_end = int(line[1])
+            cur_lattice[line[0]].append(line)
+            continue
+        if cur_lattice_end == cur_line_start:
+            if cur_lattice:
+                # if a lattice exists, add it
+                misc_field = cur_lattice_token[2] if len(cur_lattice_token) > 2 else ""
+                # print "Appending existing %s" % str((misc_field, cur_lattice, "%s-%s" % (cur_lattice_start, cur_lattice_end)))
+                lattices.append((misc_field, cur_lattice, "%s-%s" % (cur_lattice_start, cur_lattice_end)))
+            # print "Start new lattice for %s" % line[2]
+            cur_lattice = defaultdict(list)
+            cur_lattice_token = line
+            cur_lattice_start = int(line[0])
+            cur_lattice_end = int(line[1])
+            cur_lattice[line[0]].append(line)
+        else:
+            # this is an error condition
+            # print "Error"
+            # sys.exit(1)
+            return list()
     if cur_lattice:
-        lattices.append((cur_lattice_token[2], cur_lattice, cur_lattice_token[0]))
+        misc_field = cur_lattice_token[2] if len(cur_lattice_token) > 2 else ""
+        lattices.append((misc_field, cur_lattice, "%s-%s" % (cur_lattice_start, cur_lattice_end)))
+    # print "Returning lattice"
+    # pprint(lattices, width=220)
     return lattices
 
 
@@ -94,10 +150,14 @@ def conllu_as_spellouts(raw_sent):
 
 
 def isoov(lattice):
-    return 'oov' in lattice[0]
+    if lattice:
+        return 'oov' in lattice[0]
+    else:
+        return False
 
 
 extractors = [lambda v:[v[1]], lambda v:[v[1], v[3]], lambda v:[v[1], v[3], v[5]]]
+# extractors = [lambda v:[v[1], v[3], v[5]]]
 def calc_coverage(spellout_reader, lattice_reader, limit=0):
     spellout_file = depread(spellout_reader)
     lattice_file = depread(lattice_reader)
@@ -110,8 +170,13 @@ def calc_coverage(spellout_reader, lattice_reader, limit=0):
         sents += 1
         if limit and sents > limit:
             break
+        # print "At sent %d" % sents
         conllu_d = list(conllu_as_spellouts(spellout_sent))
         conllul_d = list(conllul_as_lattices(lattice_sent))
+        if len(conllul_d) == 0:
+            # print "Skipping"
+            total += len(conllu_d)
+            continue
         for spellout, lattice in zip(conllu_d, conllul_d):
             total += 1
             oov = isoov(lattice)
